525fc32342cc31bc24b84c031edba4f6fd5d6525
Joshuwa08 <joshuwa008@gmail.com>
2022-06-23 15:06:58 +0530

    1st update

---
+++ src/org/git/DAY1.java
@@ -0,0 +1,10 @@
+package org.git;
+
+public class DAY1 {
+	public static void main(String[] args) {
+		System.out.println("start");
+		System.out.println("day 1");
+		System.out.println("end");
+	}
+
+}
